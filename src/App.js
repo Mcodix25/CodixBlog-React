6c4630685960cfc014de39f4124d1faf503df3b8
mcrod <paviamcroddixon14@gmail.com>
2024-12-13 11:47:49 +0800

    new project start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+const title = " Welcome Codix"
+function App(){
+  return (
+    <div className ="App">
+      <div className="content">
+        <h1> App Component {title}</h1>
+      </div>
+    </div>
+
+  )
+}
+
+
+
+export default App;
